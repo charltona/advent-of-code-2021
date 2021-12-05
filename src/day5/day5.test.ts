7f62c2ed8f0c099ca218a5acb0735e295dd61b7b
charltona <2724511+charltona@users.noreply.github.com>
2021-12-05 14:53:20 +1000

    prep for day 5

---
+++ src/day5/day5.test.ts
@@ -0,0 +1,10 @@
+
+const testData = [];
+
+describe ('Day 5 challenges', () => {
+
+  it('does a thing', () => {
+
+    expect('a thing').toBe('a thing');
+  })
+})
---
+++ src/templates/dayX.test.ts
@@ -0,0 +1,10 @@
+
+const testData = [];
+
+describe ('Day X challenges', () => {
+
+  it('does a thing', () => {
+
+    expect('a thing').toBe('a thing');
+  })
+})
